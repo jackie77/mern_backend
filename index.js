a8dccb30035379b0bed32cbf5f466063590f99fe
Jackie L <jmba@Jackies-MacBook-Air.local>
2025-05-21 21:55:18 -0700

    fix

--- index.js
+++ index.js
@@ -4,34 +4,37 @@ import { connectDB } from "./config/db.js";
 import ProductRoutes from "./routes/product.route.js";
 import DaggerRoutes from "./routes/dagger.route.js";
 
-    const app = express()
-    dotenv.config();
+var cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+dotenv.config();
 
 //const PORT = 5009
-    app.get('/', (req, res) => {
-        res.send('Express JS on Vercel')
-    })
+app.get("/", (req, res) => {
+  res.send("Express JS on Vercel");
+});
 
-    app.get('/ping', (req, res) => { 
-        console.log("getPING")
-        res.send('pong 🏓')
-    })
+app.get("/ping", (req, res) => {
+  console.log("getPING");
+  res.send("pong 🏓");
+});
 
-    const port = process.env.PORT || 8080
-    app.use(express.json()); 
-    app.use("/api/products", ProductRoutes);
-    app.use("/api/daggers", DaggerRoutes);
+const port = process.env.PORT || 8080;
+app.use(express.json());
+app.use("/api/products", ProductRoutes);
+app.use("/api/daggers", DaggerRoutes);
 
-    console.log(process.env.MONGO_URI);
-    console.log(port)
-    
-    app.listen(port, (err, res) => {
-        connectDB()
-        if (err) {
-            console.log(err)
-            return res.status(500).send(err.message)
-        } else {
-          
-            console.log('[INFO] Server Running on port:', port)
-        }
-    })
+console.log(process.env.MONGO_URI);
+console.log(port);
+
+app.listen(port, (err, res) => {
+  connectDB();
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log("[INFO] Server Running on port:", port);
+  }
+});
